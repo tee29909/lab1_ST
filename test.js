cb465454b115bd2f118d5beb7aff43b0a0b93f4c
tee29909 <59160571@go.buu.ac.th>
2019-12-06 18:28:00 +0700

    test mpocha

--- test.js
+++ test.js
@@ -1,5 +1,46 @@
+const assert = require('assert');
 const math = require('./math');
-const add = math.add2;
+describe('file to be tested', ()=>{
+    context('function to be tested' , ()=>{
 
-console.log(math.add(50,2));
-console.log(add(6,20));
+
+
+        it('should do something', ()=>{
+            assert.equal(1, 1);
+        });
+
+        it('should do another thing', ()=>{
+            assert.deepEqual([1,2,3], [1,2,3]);
+            assert.deepEqual({name: 'kob'},{name: 'kob'});
+        });
+
+        
+
+    });
+    
+});
+
+describe('file math', ()=>{
+    context('function add' , ()=>{
+
+        
+
+        it('should do add(1+1)', ()=>{
+           assert.equal(math.add(1,1), 2);
+        });
+        it('should do add2(5+1)', ()=>{
+            assert.equal(math.add2(5,1), 6);
+         });
+         it('should do add3(10+29)', ()=>{
+            assert.equal(math.add2(10,29), 39);
+         });
+
+       
+
+        
+
+    });
+    
+});
+
+
